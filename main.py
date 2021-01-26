056b943296cff025b5fc4fd46e3885207d453e1f
Kit Fuderich <kitfuderich@gmail.com>
2021-01-26 20:51:32 +0000

    Data run, display window open on close on 'd' key press. data.png of large test run.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from pygame.locals import * 
 import random
-import time
+
 
 from virus import VirusNode 
 from doctor import Doctor
@@ -23,6 +23,7 @@ doctors = pygame.sprite.Group()
 
 round_over = False
 done = False
+display = False
 
 doc_num = 1 
 bac_num = 5 
@@ -31,18 +32,23 @@ split_time = 500
 
 success = 0
 tests = 0
+#variable used to calculate how many times bacteria can grow to during tests
 split_times = 10
-case_samples = 1
+case_samples = 10
 test_data = []
 
 def process_events(): 
-  global done
+  global display, done
   for event in pygame.event.get(): 
     if event.type == pygame.QUIT:
       done = True
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_d:  
+        display = not display
+        print("Display toggled: "+ str(display))
 
 def init():
-  global round_over 
+  global round_over
   bacteria.empty() 
   doctors.empty() 
   round_over = False
@@ -60,7 +66,8 @@ def timeout():
 
 def end_of_round():
   global tests,success,doc_num,bac_num,split_time,done
-  timeout()
+  if display:
+    timeout()
   tests +=1
   if tests >=case_samples:
     test_data.append([doc_num,bac_num,split_time,tests,success/tests*100])
@@ -73,7 +80,7 @@ def end_of_round():
       split_time = round(split_time*0.9)
       print("SPLIT TIME CURRENTLY: "+ str(split_time))
       #Below is the limiting condition on when tests complete, basically the split time for bacteria is too long in response to better doctors (equilibrium reached)
-      if split_time <400:
+      if split_time <10:
         print("all tests done!")
         done = True
     tests = 0
@@ -81,6 +88,8 @@ def end_of_round():
   init()
 
 def process_data():
+  print(test_data)
+  print("PROCESSING DATA...data.png")
   x = []
   y = []
   for row in test_data:
@@ -93,6 +102,7 @@ def process_data():
   plt.xlabel('split time (refreshes)')
   plt.ylabel('Doctors Needed')
   fig.savefig('data.png')
+ 
 
 def main():
   global success,round_over
@@ -102,10 +112,11 @@ def main():
   # doctors.add(Doctor((random.randint(50,width-50),random.randint(50,height-50))))
 
   while not done:
-    clock.tick(60) 
+    if display:
+      clock.tick(60) 
     process_events()
     if len(bacteria) > bac_num*split_times:
-    #if len(bacteria) > 8:
+    #use for testing if len(bacteria) > 8:
       text = font.render("You were overrun", True, (255, 0,0))
       text_rect =text.get_rect()
       round_over = True
@@ -114,19 +125,20 @@ def main():
       text_rect = text.get_rect()
       success +=1
       round_over = True
-      #print(round_over)
+        #print(round_over)
     else:
       doctors.update()
       bacteria.update()
       pygame.sprite.groupcollide(doctors, bacteria, False,True)
       text = font.render("Bacteria Count: {}".format(len(bacteria)), True, (255, 0, 0))
       text_rect = text.get_rect()
-
-    screen.fill(color) 
-    doctors.draw(screen) 
-    bacteria.draw(screen)
-    screen.blit(text,text_rect) 
-    pygame.display.flip()
+      
+    if display:
+      screen.fill(color) 
+      doctors.draw(screen) 
+      bacteria.draw(screen)
+      screen.blit(text,text_rect) 
+      pygame.display.flip()
     if round_over == True:
       print("end of round")
       end_of_round()
