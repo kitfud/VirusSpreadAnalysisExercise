56ca51085b4b26a77aa5a5f320c3df958f158731
Kit Fuderich <kitfuderich@gmail.com>
2021-01-26 07:15:51 +0000

    Phase 5: Data collected on tests doctors and bacteria

--- main.py
+++ main.py
@@ -20,10 +20,15 @@ color = (26, 255, 255)
 bacteria = pygame.sprite.Group() 
 doctors = pygame.sprite.Group()
 
-doc_num = 1 
-bac_num = 5 
+doc_num = 10 
+bac_num = 1 
 split_time = 500 
 done = False
+
+success = 0
+tests = 0
+case_samples = 2
+test_data = []
 
 def process_events(): 
   global done
@@ -49,11 +54,26 @@ def timeout():
     process_events()
 
 def end_of_round():
+  global tests,success,doc_num,split_time,done
   timeout()
+  tests +=1
+  if tests >=case_samples:
+    test_data.append([doc_num,bac_num,split_time,tests,success/tests*100])
+    print(test_data[-1])
+    if test_data[-1][-1] == 0:
+      doc_num +=2
+    elif test_data[-1][-1]<=75:
+      doc_num +=1
+    else:
+      split_time = round(split_time*0.9)
+      if split_time <10:
+        done = True
+    tests = 0
+    success = 0
   init()
 
 def main():
-  global round_over
+  global success,round_over
   init()
 
   bacteria.add(VirusNode((random.randint(50,width-50),random.randint(50,height-50)),split_time))
@@ -62,13 +82,15 @@ def main():
   while not done:
     clock.tick(60) 
     process_events()
-    if len(bacteria) > 9:
+    #if len(bacteria) > bac_num*split_times:
+    if len(bacteria) > 8:
       text = font.render("You were overrun", True, (255, 0,0))
       text_rect =text.get_rect()
       round_over = True
     elif len(bacteria) == 0:
       text = font.render("Outbreak stopped", True, (255, 0,0))
       text_rect = text.get_rect()
+      success +=1
       round_over = True
     else:
       doctors.update()
