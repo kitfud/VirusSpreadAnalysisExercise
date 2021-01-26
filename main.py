79122f7d1c240985a1368e63fdf813bdd26fa104
Kit Fuderich <kitfuderich@gmail.com>
2021-01-26 16:41:43 +0000

    Phase 6: Graphed the data

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import pygame
 from pygame.locals import * 
 import random
+import time
 
 from virus import VirusNode 
 from doctor import Doctor
+
+import matplotlib.pyplot as plt
+
 pygame.init()
 
 font = pygame.font.SysFont(None, 70)
-split_times = 10
-
-round_over = False
 
 size = (width, height) = (850, 480) 
 screen = pygame.display.set_mode(size) 
@@ -20,14 +21,18 @@ color = (26, 255, 255)
 bacteria = pygame.sprite.Group() 
 doctors = pygame.sprite.Group()
 
-doc_num = 10 
-bac_num = 1 
-split_time = 500 
+round_over = False
 done = False
+
+doc_num = 1 
+bac_num = 5 
+split_time = 500 
+
 
 success = 0
 tests = 0
-case_samples = 2
+split_times = 10
+case_samples = 1
 test_data = []
 
 def process_events(): 
@@ -54,7 +59,7 @@ def timeout():
     process_events()
 
 def end_of_round():
-  global tests,success,doc_num,split_time,done
+  global tests,success,doc_num,bac_num,split_time,done
   timeout()
   tests +=1
   if tests >=case_samples:
@@ -66,24 +71,41 @@ def end_of_round():
       doc_num +=1
     else:
       split_time = round(split_time*0.9)
-      if split_time <10:
+      print("SPLIT TIME CURRENTLY: "+ str(split_time))
+      #Below is the limiting condition on when tests complete, basically the split time for bacteria is too long in response to better doctors (equilibrium reached)
+      if split_time <400:
+        print("all tests done!")
         done = True
     tests = 0
     success = 0
   init()
 
+def process_data():
+  x = []
+  y = []
+  for row in test_data:
+    if row[-1] >= 75:
+      x.append(row[2])
+      y.append(row[0])
+  fig = plt.figure()
+  plt.scatter(x, y)
+  plt.title('Doctors needed to prevent an outbreak\n started by 5 bacteria')
+  plt.xlabel('split time (refreshes)')
+  plt.ylabel('Doctors Needed')
+  fig.savefig('data.png')
+
 def main():
   global success,round_over
   init()
 
-  bacteria.add(VirusNode((random.randint(50,width-50),random.randint(50,height-50)),split_time))
-  doctors.add(Doctor((random.randint(50,width-50),random.randint(50,height-50))))
+  # bacteria.add(VirusNode((random.randint(50,width-50),random.randint(50,height-50)),split_time))
+  # doctors.add(Doctor((random.randint(50,width-50),random.randint(50,height-50))))
 
   while not done:
     clock.tick(60) 
     process_events()
-    #if len(bacteria) > bac_num*split_times:
-    if len(bacteria) > 8:
+    if len(bacteria) > bac_num*split_times:
+    #if len(bacteria) > 8:
       text = font.render("You were overrun", True, (255, 0,0))
       text_rect =text.get_rect()
       round_over = True
@@ -92,6 +114,7 @@ def main():
       text_rect = text.get_rect()
       success +=1
       round_over = True
+      #print(round_over)
     else:
       doctors.update()
       bacteria.update()
@@ -104,8 +127,11 @@ def main():
     bacteria.draw(screen)
     screen.blit(text,text_rect) 
     pygame.display.flip()
-    if round_over:
+    if round_over == True:
+      print("end of round")
       end_of_round()
+    
+  process_data()
 
 if __name__ == "__main__":
   main()
